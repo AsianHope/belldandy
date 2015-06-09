051e8696406c56535549ea46956ab11c413c2291
lkozloff <lyle@asianhope.org>
2015-06-09 13:23:51 +0700

    comments

--- process.php
+++ process.php
@@ -1,14 +1,22 @@
 #!/usr/bin/php
 <?php
+        //the command that will play the bell sound
 	$command = "/usr/bin/mpg321";
+        //user to run the command as
 	$user = "root";
+        //installation directory (where to find bells.xml)
 	$source_dir = "/var/www/belldandy/";
+
+        //read all of the schedules
 	$schedules=simplexml_load_file($source_dir.'bells.xml');
+
+        //where to store the temporary cron formatted stub file we generate
 	$filename="bell.stub";
 	$fh = fopen("/tmp/".$filename,"w");
 
 	foreach($schedules as $schedule){
 	if($schedule->active =='true'){
+           //write a nice comment saying the name of the schedule
 	   fwrite($fh,"### ".$schedule->title." ###\n");
 
 	   foreach($schedule->periods->period as $period){
